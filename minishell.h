c8071c20e826abbd565bdc79514a1aae598c747c
Mohammad Alhassan <malhassa@c2r3s1.42amman.com>
2026-02-16 15:14:35 +0300

    shared struct

---
+++ minishell.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malhassa <malhassa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/14 18:45:18 by mohamed           #+#    #+#             */
+/*   Updated: 2026/02/16 15:11:06 by malhassa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+// #include <typeinfo>
+#include<unistd.h>
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdlib.h>
+#include "libft/libft.h"
+
+typedef enum e_redir_type
+{
+    R_IN,
+    R_OUT,
+    R_HEREDOC,
+    R_APPEND
+} 	redir_type;
+
+typedef struct s_redir
+{
+    int fd; // -1 default
+    char *target; // name of file
+    redir_type type;
+    struct s_redir *next_redirection;
+}t_redir;
+
+
+typedef struct s_cmd
+{
+    char			**argv;
+    t_redir		*redirs;
+    struct s_cmd	*next;
+} 	t_cmd;
+
+#endif
